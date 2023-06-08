29b3c5867720674281602b3a670327b7718bc106
chaochao-z <chao19991@hotmail.com>
2023-06-08 14:49:50 +0200

    Verif Mail debug

--- src/Security/EmailVerifier.php
+++ src/Security/EmailVerifier.php
@@ -44,7 +44,6 @@ class EmailVerifier
         $this->verifyEmailHelper->validateEmailConfirmation($request->getUri(), $user->getId(), $user->getEmail());
 
         $user->setIsVerified(true);
-
         $this->entityManager->persist($user);
         $this->entityManager->flush();
     }
