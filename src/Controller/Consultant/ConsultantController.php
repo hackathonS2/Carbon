5c791d7061d626bc281b1c917b7242cbe6de0490
boukhatem <mboukhatem777@gmail.com>
2023-06-08 17:45:26 +0200

    ProfilPage Admin Opp Consultant

---
+++ src/Controller/Consultant/ConsultantController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller\Consultant;
+use App\Repository\RecetteRepository;
+use App\Repository\UserRepository;
+use App\Repository\FavoritesRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/consultant')]
+class ConsultantController extends AbstractController
+{
+
+    #[Route('/', name: 'consultant_homepage')]
+    public function redirectToConsultant() : Response
+    {
+        return $this->redirectToRoute('consultant_home');
+    }
+
+    #[Route('/home', name: 'consultant_home')]
+    public function index_consultant(): Response
+    {
+        return $this->render('consultant/home/index.html.twig', [
+            'controller_name' => 'ConsultantController',
+        ]);
+    }
+
+    #[Route('/profil', name: 'consultant_profil')]
+    public function profile_consultant(): Response
+    {
+        return $this->render('consultant/profil/index.html.twig', [
+            'controller_name' => 'ConsultantController',
+        ]);
+    }
+
+}
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -10,23 +10,11 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
-
+#[Route('/admin')]
 class HomeController extends AbstractController
 {
-    #[Route('/consultant', name: 'consultant_only')]
-    public function redirectToConsultant() : Response
-    {
-        return $this->redirectToRoute('consultant_home');
-    }
-
-    #[Route('/admin', name: 'admin_only')]
-    public function redirectToAdmin() : Response
-    {
-        return $this->redirectToRoute('admin_home');
-    }
-
-    /*************************ADMIN*************************/
-    #[Route('/admin/home', name: 'admin_home')]
+/************************ADMIN*************************/
+    #[Route('/home', name: 'admin_home')]
     public function index(): Response
     { 
         return $this->render('admin/home/index.html.twig', [
@@ -34,15 +22,18 @@ class HomeController extends AbstractController
         ]);
     }
 
-    /********************CONSULTANT****************************/
-
-    #[Route('/consultant/home', name: 'consultant_home')]
-    public function index_consultant(): Response
+    #[Route('/', name: 'admin_only')]
+    public function redirectToAdmin() : Response
     {
-        return $this->render('consultant/home/index.html.twig', [
+        return $this->redirectToRoute('admin_home');
+    }
+
+    #[Route('/profil', name: 'admin_profil')]
+    public function profile_consultant(): Response
+    {
+        return $this->render('admin/profil/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
     }
 
-    /********************ADMIN****************************/
 }
