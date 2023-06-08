522a65013d105f41e22737bcb8460ec598992927
ronantr <noob93500@gmail.com>
2023-06-08 17:50:17 +0200

    ajoute planning et route planning

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,6 +5,7 @@ use App\Repository\RecetteRepository;
 use App\Repository\UserRepository;
 use App\Repository\FavoritesRepository;
 use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,6 +14,13 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+    private $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+    
     #[Route('/consultant', name: 'consultant_only')]
     public function redirectToConsultant() : Response
     {
@@ -24,7 +32,31 @@ class HomeController extends AbstractController
     {
         return $this->redirectToRoute('admin_home');
     }
+    #[Route('/planning', name: 'planning_corporate')]
+    public function redirectToPlanning() : Response
+    {
+        //if login
 
+        if($this->security->isGranted('IS_AUTHENTICATED_REMEMBERED')){
+
+        if($this->isGranted('ROLE_OPERATIONNEL')){
+            return $this->render('operationnel/home/planning.html.twig', [
+                'controller_name' => 'PlanningController',
+            ]);
+        }
+        if ($this->isGranted('ROLE_CONSULTANT')){
+            return $this->render('consultant/home/planning.html.twig', [
+                'controller_name' => 'PlanningController',
+            ]);
+        }
+        if ($this->isGranted('ROLE_ADMIN')){
+            return $this->render('admin/home/planning.html.twig', [
+                'controller_name' => 'PlanningController',
+            ]);
+        }
+    }else{
+        return $this->redirectToRoute('app_login');}
+    }
     /*************************ADMIN*************************/
     #[Route('/admin/home', name: 'admin_home')]
     public function index(): Response
