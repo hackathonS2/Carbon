4ff965f5a99f1faabeef1535b4e24b08e404922c
boukhatem <mboukhatem777@gmail.com>
2023-06-09 06:33:21 +0200

    Creation nouveau test

--- src/Form/TestType.php
+++ src/Form/TestType.php
@@ -2,11 +2,16 @@
 
 namespace App\Form;
 
+use App\Entity\Techno;
 use App\Entity\Test;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Validator\Constraints\Range;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 
 class TestType extends AbstractType
 {
@@ -14,14 +19,30 @@ class TestType extends AbstractType
     {
         $builder
             ->add('nom')
-            ->add('difficulte')
+            ->add('difficulte', IntegerType::class, [
+                'attr' => [
+                    'min' => 1,
+                    'max' => 3,
+                ],
+                'constraints' => [
+                    new Range([
+                        'min' => 1,
+                        'max' => 3,
+                        'notInRangeMessage' => 'La difficulté doit être comprise entre 1 et 3.',
+                    ]),
+                ],
+            ])
             ->add('actif',ChoiceType::class,[
                 'choices'  => [
                     'Actif' => true,
                     'Test Fermé' => false,
                 ],
             ])
-        ;
+            ->add('techno', EntityType::class, [
+                'class' => Techno::class,
+                'choice_label' => 'nom',
+                'mapped' => false
+            ]);            
     }
 
     public function configureOptions(OptionsResolver $resolver): void
